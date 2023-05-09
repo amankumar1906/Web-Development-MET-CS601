e0f5931fdbb89a8c3f18cadc0cd87b67fa260887
Aman Kumar <amankumar7506@gmail.com>
2023-05-09 19:49:43 -0400

    final exam commit

---
+++ Web Dev Final Exam/math/formulas.js
@@ -0,0 +1,11 @@
+export function areaParallelogram(b, h) {
+  return b * h;
+}
+
+export function areaTrapezoid(a1, b1, height1) {
+  return ((a1 + b1) / 2) * height1;
+}
+
+export function areaCylinder(radius, height) {
+  return 2 * 3.14 * radius * height + 2 * 3.14 * radius * radius;
+}
